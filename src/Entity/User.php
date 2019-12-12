157156e24fba5e1b58e7b4390a6ebe880b658edf
Evgeny <fieldebox@gmail.com>
2019-12-12 14:31:15 +0200

    added registration controller

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,89 +3,112 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class User
+class User implements UserInterface
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $Name;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $Name;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $Surname;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $Surname;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $email;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $password;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+      	{
+      		return $this->id;
+      	}
 
-    public function getName(): ?string
-    {
-        return $this->Name;
-    }
+	public function getName(): ?string
+      	{
+      		return $this->Name;
+      	}
 
-    public function setName(string $Name): self
-    {
-        $this->Name = $Name;
+	public function setName(string $Name): self
+      	{
+      		$this->Name = $Name;
+      
+      		return $this;
+      	}
 
-        return $this;
-    }
+	public function getSurname(): ?string
+      	{
+      		return $this->Surname;
+      	}
 
-    public function getSurname(): ?string
-    {
-        return $this->Surname;
-    }
+	public function setSurname(string $Surname): self
+      	{
+      		$this->Surname = $Surname;
+      
+      		return $this;
+      	}
 
-    public function setSurname(string $Surname): self
-    {
-        $this->Surname = $Surname;
+	public function getEmail(): ?string
+      	{
+      		return $this->email;
+      	}
 
-        return $this;
-    }
+	public function setEmail(string $email): self
+      	{
+      		$this->email = $email;
+      
+      		return $this;
+      	}
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+	public function getPassword(): ?string
+      	{
+      		return $this->password;
+      	}
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+	public function setPassword(string $password): self
+      	{
+      		$this->password = $password;
+      
+      		return $this;
+      	}
 
-        return $this;
-    }
+	public function getUsername()
+      	{
+      		return;
+      	}
 
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
+	public function eraseCredentials()
+      	{
+      		return;
+      	}
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+	public function getRoles()
+      	{
+      		return;
+      	}
 
-        return $this;
-    }
+	public function getSalt()
+      	{
+      		return;
+      	}
 }
